f8d138617ce3fdb08598c23f0792455e701131a8
James Mack <jdmack@ucsd.edu>
2014-04-23 02:27:48 +0000

    Add base Deck class

---
+++ src/Deck.java
@@ -0,0 +1,22 @@
+public class Deck 
+{
+    public int [] deckArray;
+    public int deckPosition;
+
+    /* This is a constructor. This function is called when you do:
+            Deck thisDeck = new Deck();    
+
+            that Deck(); is calling this function to create the Deck object
+            So any intialization code for a Deck object goes in this constructor
+    */
+    public Deck()
+    {
+        deckPosition = 0;
+    }
+
+    public int dealCard()
+    {
+        // put deal function here
+    }
+
+}
